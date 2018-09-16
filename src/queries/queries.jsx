ae91a2d4d4741c62cd81b96de321df036c3d58b1
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-09-16 00:00:30 -0300

    external file for queries

---
+++ src/queries/queries.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { gql } from 'apollo-boost'
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+const getAuthorsQuery = gql`
+  {
+    authors {
+      name
+      id
+    }
+  }
+`;
+
+export { getBooksQuery, getAuthorsQuery }
