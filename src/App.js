c5c132ab86775a5435194ce52a8184bbe4f9e66a
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-09-15 22:56:24 -0300

    booklist component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import BookList from './components/BookList'
+
 
 class App extends Component {
   render() {
     return (
       <div className="main">
         <h1> Nicollas Reading List</h1>
+        <BookList />
       </div>
     );
   }
